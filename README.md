28f22cfc1d8e038cef4ea745d86c3e5e9269fb27
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2020-10-26 11:35:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Teste_Nerau-Frontend
+ 
---
+++ js/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
